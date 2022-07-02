a54f7ca11be3309ecf340847c7c729510e955cf8
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-02 23:03:17 +0100

    fixed typo

--- src/containers/GamesBox.jsx
+++ src/containers/GamesBox.jsx
@@ -9,20 +9,6 @@ const GameBox = () => {
     const [genres, setGenres] = useState([]);
     const [results, setResults] = useState([]);
 
-    const options = {
-        title: "My Daily Activities",
-    };
-    const data = [
-        ["Task", "Hours per Day"],
-        ["Work", 11],
-        ["Eat", 2],
-        ["Commute", 2],
-        ["Watch TV", 2],
-        ["Sleep", 7],
-    ];
-
-      
-
     const getGames = () => {
         fetch(`https://www.freetogame.com/api/games`)
         .then(result => result.json())
@@ -60,7 +46,6 @@ const GameBox = () => {
             <GenreSelect genres={genres} onSelectGenre={onSelectGenre}/>
             {results.length === 0 ? <FilterResults results={games}/> : <FilterResults results={results}/>}
             <GenresPieChart games={games} genres={genres}/>
-            {/* <Chart chartType="PieChart" data={data} options={options} width={"100%"} height={"400px"}/> */}
         </div>
     );
 };
